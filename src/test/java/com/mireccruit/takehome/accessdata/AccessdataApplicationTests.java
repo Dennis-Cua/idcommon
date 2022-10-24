53d5c3ce324975fd417a37ea408d8546974d1e2c
Dennis-Cua <78981458+Dennis-Cua@users.noreply.github.com>
2022-10-24 11:43:45 +0800

    Add files via upload

--- src/test/java/com/mireccruit/takehome/accessdata/AccessdataApplicationTests.java
+++ src/test/java/com/mireccruit/takehome/accessdata/AccessdataApplicationTests.java
@@ -1,13 +1,92 @@
 package com.mireccruit.takehome.accessdata;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mireccruit.takehome.accessdata.domain.Task;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class AccessdataApplicationTests {
+
+	@Autowired
+	private MockMvc mvc;
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	public void getTaskListAPI() throws Exception
+	{
+		mvc.perform( MockMvcRequestBuilders
+						.get("/GetTaskList")
+						.accept(MediaType.APPLICATION_JSON))
+				.andDo(print())
+				.andExpect(status().isOk())
+				.andExpect(MockMvcResultMatchers.jsonPath("$.task").exists())
+				.andExpect(MockMvcResultMatchers.jsonPath("$.task[*].id").isNotEmpty());
+	}
+
+	@Test
+	public void getTaskByIDAPI() throws Exception
+	{
+		mvc.perform( MockMvcRequestBuilders
+						.get("/GetTask/{id}", 1)
+						.accept(MediaType.APPLICATION_JSON))
+				.andDo(print())
+				.andExpect(status().isOk())
+				.andExpect(MockMvcResultMatchers.jsonPath("$.id").value(1));
+	}
+
+	@Test
+	public void newTaskAPI() throws Exception
+	{
+		mvc.perform( MockMvcRequestBuilders
+						.put("/newTask ")
+						.content(asJsonString(new Task("Task 1", "Doing a first task",true)))
+						.contentType(MediaType.APPLICATION_JSON)
+						.accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isCreated())
+				.andExpect(MockMvcResultMatchers.jsonPath("$.employeeId").exists());
+	}
+
+	@Test
+	public void updateTaskAPI() throws Exception
+	{
+		mvc.perform( MockMvcRequestBuilders
+						.post("/updateTask/{id}", 2)
+						.content(asJsonString(new Task(2, "Task 2", "Accompliishing second task" ,true)))
+						.contentType(MediaType.APPLICATION_JSON)
+						.accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andExpect(MockMvcResultMatchers.jsonPath("$.name").value("Task 2"))
+				.andExpect(MockMvcResultMatchers.jsonPath("$.description").value("Accompliishing second task"))
+				.andExpect(MockMvcResultMatchers.jsonPath("$.status").value(true));
+	}
+
+	@Test
+	public void deleteTaskAPI() throws Exception
+	{
+		mvc.perform( MockMvcRequestBuilders.delete("/deleteTask/{id}", 1) )
+				.andExpect(status().isAccepted());
+	}
+
+	public static String asJsonString(final Object obj) {
+		try {
+			return new ObjectMapper().writeValueAsString(obj);
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
 	}
 
 }
